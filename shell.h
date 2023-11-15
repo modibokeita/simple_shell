ebce6ae90f8183dda40efd6eabc3c8e81b422d6e
Modibo Keita <modibokeita9390@gmail.com>
2023-11-15 08:02:56 -0800

    header files

--- shell.h
+++ shell.h
@@ -138,8 +138,8 @@ char *_starts_with(const char *, const char *);
 char *_str_cat(char *, char *);
 
 /* toem_string1.c */
-char *_strcpy(char *, char *);
-char *_strdup(const char *);
+char *_str_cpy(char *, char *);
+char *_str_dup(const char *);
 void _puts(char *);
 int _putchar(char);
 
@@ -149,16 +149,16 @@ char *_strn_cat(char *, char *, int);
 char *_str_chr(char *, char);
 
 /* toem_tokenizer.c */
-char **strtow(char *, char *);
-char **strtow2(char *, char);
+char **_strtow(char *, char *);
+char **_strtow2(char *, char);
 
 /* toem_realloc.c */
-char *_memset(char *, char, unsigned int);
-void ffree(char **);
-void *_realloc(void *, unsigned int, unsigned int);
+char *is_memset(char *, char, unsigned int);
+void _ffree(char **);
+void *is_realloc(void *, unsigned int, unsigned int);
 
 /* toem_memory.c */
-int bfree(void **);
+int _bfree(void **);
 
 /* toem_atoi.c */
 int mode_interactive(info_t *);
@@ -167,69 +167,69 @@ int _is_alpha(int);
 int _a_toi(char *);
 
 /* toem_errors1.c */
-int _erratoi(char *);
-void print_error(info_t *, char *);
-int print_d(int, int);
-char *convert_number(long int, int, int);
-void remove_comments(char *);
+int is_erratoi(char *);
+void _print_error(info_t *, char *);
+int _print_d(int, int);
+char *_convert_number(long int, int, int);
+void _remove_comments(char *);
 
 /* toem_builtin.c */
-int _myexit(info_t *);
-int _mycd(info_t *);
-int _myhelp(info_t *);
+int is_myexit(info_t *);
+int is_mycd(info_t *);
+int is_myhelp(info_t *);
 
 /* toem_builtin1.c */
-int _myhistory(info_t *);
-int _myalias(info_t *);
+int is_myhistory(info_t *);
+int is_myalias(info_t *);
 
 /*toem_getline.c */
-ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
-void sigintHandler(int);
+ssize_t _get_input(info_t *);
+int is_getline(info_t *, char **, size_t *);
+void _sigintHandler(int);
 
 /* toem_getinfo.c */
-void clear_info(info_t *);
-void set_info(info_t *, char **);
-void free_info(info_t *, int);
+void _clear_info(info_t *);
+void _set_info(info_t *, char **);
+void _free_info(info_t *, int);
 
 /* toem_environ.c */
-char *_getenv(info_t *, const char *);
-int _myenv(info_t *);
-int _mysetenv(info_t *);
-int _myunsetenv(info_t *);
-int populate_env_list(info_t *);
+char *is_getenv(info_t *, const char *);
+int is_myenv(info_t *);
+int is_mysetenv(info_t *);
+int is_myunsetenv(info_t *);
+int _populate_env_list(info_t *);
 
 /* toem_getenv.c */
-char **get_environ(info_t *);
-int _unsetenv(info_t *, char *);
-int _setenv(info_t *, char *, char *);
+char **_get_environ(info_t *);
+int is_unsetenv(info_t *, char *);
+int is_setenv(info_t *, char *, char *);
 
 /* toem_history.c */
-char *get_history_file(info_t *info);
-int write_history(info_t *info);
-int read_history(info_t *info);
-int build_history_list(info_t *info, char *buf, int linecount);
-int renumber_history(info_t *info);
+char *_get_history_file(info_t *info);
+int _write_history(info_t *info);
+int _read_history(info_t *info);
+int _build_history_list(info_t *info, char *buf, int linecount);
+int _renumber_history(info_t *info);
 
 /* toem_lists.c */
-list_t *add_node(list_t **, const char *, int);
-list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
-int delete_node_at_index(list_t **, unsigned int);
-void free_list(list_t **);
+list_t *_add_node(list_t **, const char *, int);
+list_t *_add_node_end(list_t **, const char *, int);
+size_t _print_list_str(const list_t *);
+int _delete_node_at_index(list_t **, unsigned int);
+void _free_list(list_t **);
 
 /* toem_lists1.c */
-size_t list_len(const list_t *);
-char **list_to_strings(list_t *);
-size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
+size_t _list_len(const list_t *);
+char **_list_to_strings(list_t *);
+size_t _print_list(const list_t *);
+list_t *_node_starts_with(list_t *, char *, char);
+ssize_t _get_node_index(list_t *, list_t *);
 
 /* toem_vars.c */
-int is_chain(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
-int replace_vars(info_t *);
-int replace_string(char **, char *);
+int is_a_chain(info_t *, char *, size_t *);
+void _check_chain(info_t *, char *, size_t *, size_t, size_t);
+int _replace_alias(info_t *);
+int _replace_vars(info_t *);
+int _replace_string(char **, char *);
 
 #endif
